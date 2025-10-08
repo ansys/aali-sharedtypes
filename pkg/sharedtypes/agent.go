a9ba399c4bcf190d6dc9fe4ea90a6f796a8eedf2
FelixKuhnAnsys <felix.kuhn@ansys.com>
2025-10-08 10:13:44 +0200

    add api key and ip address to grpc

--- pkg/sharedtypes/agent.go
+++ pkg/sharedtypes/agent.go
@@ -30,12 +30,14 @@ import (
 
 // Message represents the JSON message you are expecting
 type SessionContext struct {
-	SessionType string            `json:"session_type"`          // Type of session: "workflow", "exec"
-	ApiKey      string            `json:"api_key"`               // API key for authentication, only relevant if "session_type" is "exec"
-	JwtToken    string            `json:"jwt_token"`             // JWT token for authentication (optional)
-	ExecId      string            `json:"exec_id,omitempty"`     // Unique identifier of connecting Exec, only relevant if "session_type" is "exec"
-	WorkflowId  string            `json:"workflow_id,omitempty"` // Workflow ID, only relevant if "workflow_endpoint" is "custom"
-	Variables   map[string]string `json:"variables,omitempty"`   // Variables to be passed to the workflow
+	WorkflowId string            `json:"workflow_id,omitempty"` // Workflow ID, only relevant if "workflow_endpoint" is "custom"
+	Variables  map[string]string `json:"variables,omitempty"`   // Variables to be passed to the workflow
+	// Exec connection
+	SessionType string `json:"session_type"`      // Type of session: "workflow", "exec"; default is "workflow" if not provided
+	ExecId      string `json:"exec_id,omitempty"` // Unique identifier of connecting Exec, only relevant if "session_type" is "exec"
+	// Authentication
+	JwtToken string `json:"jwt_token"` // JWT token for authentication for "session_type" "workflow" (optional)
+	ApiKey   string `json:"api_key"`   // API key for authentication, used if "session_type" is "exec" or can be used if "session_type" is "workflow" for authentication
 	// Snapshot logic
 	SnapshotId     string `json:"snapshot_id,omitempty"`     // Snapshot ID, only relevant if "session_type" is "workflow"; if defined, the given snapshot will retrived from the database
 	WorkflowRunId  string `json:"workflow_run_id,omitempty"` // Workflow run ID, only relevant if "session_type" is "workflow"; if defined, mandatory if "snapshot_id" is defined in order to retrieve the snapshot from the database
