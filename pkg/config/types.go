323164079c8e8f3abd870c2bcf6ad7a76103af93
Ryan Crisanti <ryan.crisanti@ansys.com>
2026-01-05 09:50:07 -0500

    Revert "feat: add `LLM_PROMPTS_FILE` config setting" (#247)

--- pkg/config/types.go
+++ pkg/config/types.go
@@ -115,7 +115,6 @@ type Config struct {
 	LLM_ADDRESS            string `yaml:"LLM_ADDRESS" json:"LLMADDRESS"`
 	MODELS_CONFIG_LOCATION string `yaml:"MODELS_CONFIG_LOCATION" json:"MODELSCONFIGLOCATION"`
 	LLM_API_KEY            string `yaml:"LLM_API_KEY" json:"LLMAPIKEY"`
-	LLM_PROMPTS_FILE       string `yaml:"LLM_PROMPTS_FILE" json:"LLMPROMPTSFILE"`
 
 	// Aali Exec
 	//////////////
