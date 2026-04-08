09bdde336abcf2561c78e369dee07a0e9781580c
tianye.song <tianye.song@ansys.com>
2026-04-08 13:48:22 -0700

    feature: Add chat address to the config structure

--- pkg/config/types.go
+++ pkg/config/types.go
@@ -60,11 +60,11 @@ type Config struct {
 	EXTRACT_CONFIG_FROM_AZURE_KEY_VAULT bool   `yaml:"EXTRACT_CONFIG_FROM_AZURE_KEY_VAULT" json:"EXTRACTCONFIGFROMAZUREKEYVAULT"`
 	AZURE_KEY_VAULT_NAME                string `yaml:"AZURE_KEY_VAULT_NAME" json:"AZUREKEYVAULTNAME"`
 	AZURE_MANAGED_IDENTITY_ID           string `yaml:"AZURE_MANAGED_IDENTITY_ID" json:"AZUREMANAGEDIDENTITYID"`
-    
-    // Aali Chat
-    ///////////////
-    CHAT_ADDRESS string `yaml:"CHAT_ADDRESS" json:"CHATADDRESS"`
-	
+
+	// Aali Chat
+	///////////////
+	CHAT_ADDRESS string `yaml:"CHAT_ADDRESS" json:"CHATADDRESS"`
+
 	// Aali Agent
 	///////////////
 	AGENT_ADDRESS    string `yaml:"AGENT_ADDRESS" json:"AGENTADDRESS"`
